38a21e02a7f37900311cd6ace4f22c0d8ddbd009
Julio González <juliogtrenard@gmail.com>
2024-11-06 19:10:18 +0100

    ADD no redimensionar la ventana.

--- src/main/java/es/juliogtrenard/cambiocoche/HelloApplication.java
+++ src/main/java/es/juliogtrenard/cambiocoche/HelloApplication.java
@@ -3,20 +3,36 @@ package es.juliogtrenard.cambiocoche;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
+/**
+ * Clase donde se ejecuta la aplicación
+ */
 public class HelloApplication extends Application {
+    /**
+     * Muestra la ventana de la aplicación.
+     *
+     * @param stage La ventana
+     */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/es/juliogtrenard/cambiocoche/fxml/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("MINI COOPER");
+        stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("/es/juliogtrenard/cambiocoche/img/Cooper.png"))));
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Lanza la aplicación
+     * @param args Argumentos de linea de comandos
+     */
     public static void main(String[] args) {
         launch();
     }
